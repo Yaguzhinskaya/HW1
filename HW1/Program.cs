ff1c93cad3ba467542b0b50fbc8204a559c399f1
Настенька <nastenka.kuzmina.1996@mail.ru>
2022-11-23 20:25:05 +0300

    HW1

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Input number 1");
+// Console.WriteLine("Input number 2");
+// int a = Convert.ToInt32(Console.ReadLine()); 
+// int b = Convert.ToInt32(Console.ReadLine()); 
+
+// Console.WriteLine(a);
+// Console.WriteLine(b);
+
+/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+/*Console.WriteLine("Здравствуйте! чтобы вычислить наибольшее число то");
+Console.WriteLine("введите число 1");
+Console.WriteLine("введите число 2");
+
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+if (a > b)
+{
+    Console.WriteLine($"{a}");
+}
+if (a < b)
+{
+    Console.WriteLine($"{b}");
+}
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+/*Console.WriteLine("Ведите три числа и я покажу вам наибольшее");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+int n3 = Convert.ToInt32(Console.ReadLine());
+int max = n1;
+if (n2 > max)
+{
+    max = n2;
+}
+if (n3 > max)
+{
+    max = n3;
+}
+Console.WriteLine(max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+/*Console.WriteLine("Ведите одно число, а я вам подскажу четное оно или нет");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+if (num % 2 == 1)
+{
+    Console.WriteLine("не четное");
+}
+Console.WriteLine(num);
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+/*Console.WriteLine("Ведите одно число. Покажу вам четные");
+int num = Convert.ToInt32(Console.ReadLine());
+int co = 2;
+while(co <= num)
+{
+    Console.WriteLine(co);
+    co += 2;
+}
+*/
