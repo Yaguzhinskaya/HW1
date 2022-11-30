acfe48e22297255fca9cc2f3d19f5e2aa379f774
Настенька <nastenka.kuzmina.1996@mail.ru>
2022-11-30 22:12:28 +0300

    HW7

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -4,7 +4,42 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+/*int[,] RandomArray(int rows, int columns, int min, int max)
+{
+    int[,] newArray = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            newArray[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return newArray;
+}
 
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное значение элемента");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальное значение элемента");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Print2DArray(RandomArray(rows, columns, min, max));
+*/
 
 
 
@@ -23,7 +58,49 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
+/*
+int rows = ReadInt("Введите число строки: ");
+int colums = ReadInt("Введите число столбца: ");
+int[,] numbers = new int[6, 8];
+MayArray(numbers);
+MayToArray2D(numbers);
 
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) 
+Console.WriteLine(numbers[rows, colums]);
+else 
+Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+void MayArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void MayToArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+*/
 
 
 
@@ -39,3 +116,54 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+/*Console.WriteLine("введите номер строк");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер столбцов");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] num = new int[n, m];
+MayArrayRandomNum(num);
+
+
+for (int j = 0; j < num.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < num.GetLength(0); i++)
+    {
+        avarage = (avarage + num[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+Console.WriteLine();
+PrintArray(num);
+
+
+
+void MayArrayRandomNum(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+*/
